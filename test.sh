05f5f2f1503d6c878588651e9febae18658b7bc7
root <root@docker.lan>
2025-09-22 14:26:55 +0300

    test

--- test.sh
+++ test.sh
@@ -1,1 +1,6 @@
-curl -X POST http://localhost:8000/detect -F "image=@test_image.jpg"
+#curl -X POST http://localhost:8000/detect -F "image=@test_image.jpg"
+for image in *.jpg; do
+  echo "Processing $image"
+  curl -X POST http://localhost:8000/detect -F "image=@${image}"
+  echo -e "\n"
+done
